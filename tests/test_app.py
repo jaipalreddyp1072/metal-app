cd2a863f337cceca550a8be4904e77c18cd037a8
jaipalreddyp1072 <jaipalreddyp@nexnoratech.onmicrosoft.com>
2025-10-30 05:06:38 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
+import os
+import sys
 import pytest
+
+# Add parent folder to sys.path so imports work
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+
 
 @pytest.fixture
 def client():
@@ -7,10 +14,12 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_homepage_status(client):
-    """Check if the homepage loads successfully"""
+    """Check if homepage loads successfully"""
     response = client.get('/')
     assert response.status_code == 200
+
 
 def test_html_contains_metals(client):
     """Ensure the page contains metal names"""
@@ -18,4 +27,3 @@ def test_html_contains_metals(client):
     html = response.data.decode('utf-8')
     for metal in ['Gold', 'Silver', 'Platinum', 'Copper', 'Iron']:
         assert metal.lower() in html.lower()
-
